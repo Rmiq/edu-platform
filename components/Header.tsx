f5f965bcfe7725455b2ad76a295fcdee6b6bf678
Remigiusz Chiluta <remigiuszchiluta@192.168.1.2>
2023-01-04 20:27:13 +0100

    add discord auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
         </Link>
         <div>
           <span className="mx-2 text-lg">💰: {points}</span>
-          <Link className="mx-2 text-lg" href="/account">👩 {user?.email}</Link>
+          <Link className="mx-2 text-lg" href="/account">{user?.avatar_url} {user?.email}</Link>
         </div>
       </div>
     </header>
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -20,7 +20,7 @@ const SignIn = () => {
       <main className="container mx-auto max-w-2xl py-8">
         <h1 className="text-2xl text-center py-4">Please login to the application</h1>
         {!session ? (
-          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} redirectTo="/" />
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} redirectTo="/" providers={["discord"]}/>
         ) : null}
       </main>
     </div>
